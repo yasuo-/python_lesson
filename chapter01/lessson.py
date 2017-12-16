b531d3fa3aad9d8bb9b51eb5bf68e7fb8556f19b
yasuo <breitling1216@gmail.com>
2017-12-17 00:20:05 +0900

    study 1

---
+++ chapter01/lessson.py
@@ -0,0 +1,26 @@
+
+x = -10
+
+
+if x < 0:
+    print('negata')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('10')
+else:
+    print('positive')
+
+
+a = 1
+b = 1
+
+print(a == b)
+
+print(a != b)
+
+if a > 0 and b > 0:
+    print('positive')
+
+if a > 0 or b > 0:
+    print('or')
