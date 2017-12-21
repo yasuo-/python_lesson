0fb100aca4e993dc172c1e6023d25e548ff021aa
yasuo <breitling1216@gmail.com>
2017-12-21 09:05:48 +0900

    namespace & scope add

---
+++ chapter01/lesson_namespace.py
@@ -0,0 +1,18 @@
+"""
+namespace & scope comment
+ - name_space function dose not load global x
+"""
+
+x = "dog"
+
+
+def name_space():
+    # print(x)  globalのxは呼べない
+    x = "cat"
+    print("local:",x)
+    print("locals():", locals())
+
+
+name_space()
+print("global:", x)
+print("globals():", globals())
